a7f9b892f1f492b60d70613753eabf00ef9394c1
Wiley <wiley.finch@inholland.nl>
2023-01-12 09:02:47 +0100

    initial

---
+++ manage.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from flask_migrate import Migrate
+
+from app import blueprint
+from app.main import create_app, db
+from app.main.model import user, blacklist
+
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+app.register_blueprint(blueprint)
+
+app.app_context().push()
+
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=user, Blacklist=blacklist)
+
+@app.cli.command()
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
